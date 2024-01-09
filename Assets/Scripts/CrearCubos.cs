aece85160de0453688556bd7f3c7a8623ae7f13f
Pablo Maxiá <pmaxiarodriguez@gmail.com>
2024-01-09 18:46:08 +0100

    Update CrearCubos.cs

--- Assets/Scripts/CrearCubos.cs
+++ Assets/Scripts/CrearCubos.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CrearCubos : MonoBehaviour
 {
+    public GameObject cubo;
+    public float tiempoGeneracion;
+
+    private int limiteX = 10;
+    private int limiteY = 10;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,15 @@ public class CrearCubos : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void generarCubo()
+    {
+        System.Random random = new System.Random();
+
+        float posicionX = random.Next(-limiteX, limiteX + 1);
+
+        cubo.transform.position = new Vector3(posicionX, limiteY, transform.position.z);
+        Instantiate(cubo);
     }
 }
